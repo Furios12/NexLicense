7e3f03c31d70bbe4b7f29afae7ba437b8fcd8f62
Furios12 <102328541+Furios12@users.noreply.github.com>
2025-03-10 13:15:09 +0100

    Update route.ts

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,5 +3,5 @@ import { cookies } from "next/headers";
 
 export async function POST() {
   (await cookies()).delete("session_id");
-  return NextResponse.json({ success: true }); //
+  return NextResponse.json({ success: true }); 
 }
